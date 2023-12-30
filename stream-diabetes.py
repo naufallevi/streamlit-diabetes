66f4e950fd3d007c7022bae321da0bcb033ac593
naufallevi <155075681+naufallevi@users.noreply.github.com>
2023-12-30 10:38:09 +0700

    Update stream-diabetes.py

--- stream-diabetes.py
+++ stream-diabetes.py
@@ -1,50 +1,65 @@
 import pickle
-import streamlit as st 
+import streamlit as st
 
 # Load Model
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-# Judul
+# Set up background image
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://example.com/background_image.jpg") no-repeat center center;
+        background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Judul dan deskripsi
 st.title("Prediksi Data Mining Diabetes")
+st.write(
+    "Isi formulir di bawah ini dengan informasi pasien untuk melakukan prediksi apakah pasien terkena diabetes atau tidak."
+)
 
 # Membagi Kolom
-col1, col2 =st.columns(2)
+col1, col2 = st.columns(2)
 
 with col1:
-    Pregnancies = st.number_input("Input Nilai Pregnancies/ Kehamilan", step=1, value=0)
+    Pregnancies = st.number_input("Pregnancies / Kehamilan", min_value=0, max_value=17, value=0)
 
 with col2:
-    Glucose = st.number_input("Input Nilai Glucose/ Glukosa")
+    Glucose = st.number_input("Glucose / Glukosa", min_value=0, max_value=200, value=0)
 
 with col1:
-    BloodPressure = st.number_input("Input Nilai Blood Pressure/ Tekanan Darah")
+    BloodPressure = st.number_input("Blood Pressure / Tekanan Darah", min_value=0, max_value=122, value=0)
 
 with col2:
-    SkinThickness = st.number_input("Input Nilai Skin Thickness/ Ketebalan Kulit")
+    SkinThickness = st.number_input("Skin Thickness / Ketebalan Kulit", min_value=0, max_value=99, value=0)
 
 with col1:
-    Insulin = st.number_input("Input Nilai Insulin")
+    Insulin = st.number_input("Insulin", min_value=0, max_value=846, value=0)
 
 with col2:
-    BMI = st.number_input("Input Nilai BMI")
+    BMI = st.number_input("BMI", min_value=0.0, max_value=67.1, value=0.0)
 
 with col1:
-    DiabetesPedigreeFunction = st.number_input("Input Nilai Diabetes Pedigree Function/ Fungsi Silsilah Diabetes")
+    DiabetesPedigreeFunction = st.number_input("Diabetes Pedigree Function / Fungsi Silsilah Diabetes", min_value=0.078, max_value=2.42, value=0.078)
 
 with col2:
-    Age = st.number_input("Input Nilai Age/ Usia")
+    Age = st.number_input("Age / Usia", min_value=21, max_value=81, value=21)
 
 # Kode Prediksi
-
 diab_diagnosis = ''
 
 # Membuat Tombol Prediksi
 if st.button('Test Prediksi'):
-    diab_prediction = diabetes_model.predict([[Pregnancies,Glucose,BloodPressure,
-                                               SkinThickness,Insulin,BMI,
-                                               DiabetesPedigreeFunction,Age]])
+    diab_prediction = diabetes_model.predict([[Pregnancies, Glucose, BloodPressure,
+                                               SkinThickness, Insulin, BMI,
+                                               DiabetesPedigreeFunction, Age]])
 
-    if(diab_prediction[0] == 1):
+    if diab_prediction[0] == 1:
         diab_diagnosis = "Pasien Terkena Diabetes"
     else:
         diab_diagnosis = "Pasien Tidak Terkena Diabetes"
